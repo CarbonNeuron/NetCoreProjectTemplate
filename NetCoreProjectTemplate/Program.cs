0d3e1b6c6664ed75035ffff42a088520d76ca667
Carbon <robotcreeper13@gmail.com>
2020-10-15 12:19:07 -0500

    Add the new console app

---
+++ NetCoreProjectTemplate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCoreProjectTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
